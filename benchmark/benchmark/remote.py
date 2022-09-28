e45d34f13911463cec8fa48a3cfb76b977b17d51
sh1hsh1nk <shashank.motepalli@mail.utoronto.ca>
2022-09-28 17:14:22 +0530

    typo location

--- benchmark/benchmark/remote.py
+++ benchmark/benchmark/remote.py
@@ -94,7 +94,7 @@ class Bench:
             raise BenchError('Failed to kill nodes', FabricError(e))
 
     def _select_hosts(self):
-        f = open('../../../IP.txt', 'r+')
+        f = open('../../IP.txt', 'r+')
         addrs = [line.strip() for line in f.readlines()]
         f.close()
         return addrs
